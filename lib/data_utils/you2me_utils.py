a25e5d84696ccce89cf3808e4d50a2e0690c1676
AlexanderNevada <alexander.qi.eins@gmail.com>
2022-04-13 16:21:36 +0200

    try one lesscatch data

--- lib/data_utils/you2me_utils.py
+++ lib/data_utils/you2me_utils.py
@@ -24,10 +24,10 @@ from lib.utils.smooth_bbox import get_smooth_bbox_params, get_all_bbox_params
 from lib.data_utils._occ_utils import load_occluders
 
 VIS_THRESH = 0.3
-# '11-hand2', '12-hand3',
+# '11-hand2', '12-hand3', '3-catch3'
 cmu_train_list = ['1-catch1', '10-hand1',  
                 '13-sports1', '14-sports2', 
-                '2-catch2', '3-catch3', 
+                '2-catch2',
                 '4-convo1', '5-convo2', '6-convo3', '7-convo4', '8-convo5', '9-convo6']
 
 kinect_train_list = ['catch36', 'catch37', 'catch39', 'catch40', 'catch41', 'catch42', 'catch55', 
