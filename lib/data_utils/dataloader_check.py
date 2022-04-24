05c8d7eae6cd2c4e6430c3e84ac97450a3226ccf
AlexanderNevada <alexander.qi.eins@gmail.com>
2022-04-24 21:33:22 +0200

    check orgin of ground truth

--- lib/data_utils/dataloader_check.py
+++ lib/data_utils/dataloader_check.py
@@ -10,6 +10,6 @@ db_file = osp.join(db_path, 'you2me_train_db_kinect.pt')
 db = joblib.load(db_file) 
 joints3D_np = db['joints3D']
 print(np.shape(joints3D_np))
-print('len(joints3D_np[0,25:39,:])',len(joints3D_np[0,25:39,:]))
+print('len(joints3D_np[0,25:39,:])',len(joints3D_np[0,25:40,:]))
 
-print('joints3D_np example', joints3D_np[0,25:39,:])
+print('joints3D_np example', joints3D_np[0,25:40,:])
