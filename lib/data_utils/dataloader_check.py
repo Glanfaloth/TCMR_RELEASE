ab78f4f8d0e819cf561c5dc3dc81a2277e0d0e95
AlexanderNevada <alexander.qi.eins@gmail.com>
2022-04-24 21:36:36 +0200

    draw dataloader 3d

--- lib/data_utils/dataloader_check.py
+++ lib/data_utils/dataloader_check.py
@@ -2,6 +2,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import os.path as osp
+import numpy as np
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import json
+
 import joblib
 
 
@@ -13,3 +18,7 @@ print(np.shape(joints3D_np))
 print('len(joints3D_np[0,25:39,:])',len(joints3D_np[0,25:40,:]))
 
 print('joints3D_np example', joints3D_np[0,25:40,:])
+
+fig = plt.figure(figsize=plt.figaspect(0.5))
+ax2 = fig.add_subplot(2, 1, 1, projection='3d') 
+ax2.scatter(joints3D_np[:,:-1,0],joints3D_np[:,:-1,1],joints3D_np[:,:-1,2])
