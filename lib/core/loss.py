0667b2e061fa20810630675da2bb7ef24d834bcb
Your Name <moonlitfallie@163.com>
2022-05-11 14:23:11 +0200

    update loss

--- lib/core/loss.py
+++ lib/core/loss.py
@@ -203,17 +203,22 @@ class TCMRLoss(nn.Module):
         # gt_keypoints_3d = gt_keypoints_3d[:, :, :-1].clone()
         # gt_keypoints_3d = gt_keypoints_3d
         # conf = conf
-        pred_keypoints_3d = pred_keypoints_3d
-        if len(gt_keypoints_3d) > 0:
-            gt_pelvis = (gt_keypoints_3d[:, 2,:] + gt_keypoints_3d[:, 3,:]) / 2
-            gt_keypoints_3d = gt_keypoints_3d - gt_pelvis[:, None, :]
-            pred_pelvis = (pred_keypoints_3d[:, 2,:] + pred_keypoints_3d[:, 3,:]) / 2
-            pred_keypoints_3d = pred_keypoints_3d - pred_pelvis[:, None, :]
-            # print(conf.shape, pred_keypoints_3d.shape, gt_keypoints_3d.shape)
-            # return (conf * self.criterion_keypoints(pred_keypoints_3d, gt_keypoints_3d)).mean()
-            return self.criterion_keypoints(pred_keypoints_3d, gt_keypoints_3d).mean()
-        else:
-            return torch.FloatTensor(1).fill_(0.).to(self.device)
+        # pred_keypoints_3d = pred_keypoints_3d
+        # if len(gt_keypoints_3d) > 0:
+        #     gt_pelvis = (gt_keypoints_3d[:, 2,:] + gt_keypoints_3d[:, 3,:]) / 2
+        #     gt_keypoints_3d = gt_keypoints_3d - gt_pelvis[:, None, :]
+        #     pred_pelvis = (pred_keypoints_3d[:, 2,:] + pred_keypoints_3d[:, 3,:]) / 2
+        #     pred_keypoints_3d = pred_keypoints_3d - pred_pelvis[:, None, :]
+        #     # print(conf.shape, pred_keypoints_3d.shape, gt_keypoints_3d.shape)
+        #     # return (conf * self.criterion_keypoints(pred_keypoints_3d, gt_keypoints_3d)).mean()
+        #     return self.criterion_keypoints(pred_keypoints_3d, gt_keypoints_3d).mean()
+        # else:
+        #     return torch.FloatTensor(1).fill_(0.).to(self.device)
+        S1_hat = batch_compute_similarity_transform_torch(pred_keypoints_3d, gt_keypoints_3d)
+        mpjpe_pa = torch.sqrt(((S1_hat - gt_keypoints_3d) ** 2).sum(dim=-1))
+        # mpjpe_pa = mpjpe_pa.mean(axis=-1)
+        mpjpe_pa = mpjpe_pa.mean()
+        return mpjpe_pa
 
     def smpl_losses(self, pred_rotmat, pred_betas, gt_pose, gt_betas):
         pred_rotmat_valid = batch_rodrigues(pred_rotmat.reshape(-1,3)).reshape(-1, 24, 3, 3)
