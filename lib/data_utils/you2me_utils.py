5cb5207edca2607d0ca1204ec559c462318115ec
AlexanderNevada <alexander.qi.eins@gmail.com>
2022-05-20 11:50:44 +0200

    try old dataloader

--- lib/data_utils/you2me_utils.py
+++ lib/data_utils/you2me_utils.py
@@ -227,29 +227,29 @@ def read_val_data(dataset_path, device, data_type, debug=False):
                 #     bbox = get_bbox_from_kp2d(joints_2d[~np.all(joints_2d == 0, axis=1)]).reshape(4)
                 bbox = np.array([113, 113, w, h])  # shape = (4,N)
 
-                if i == 0: 
-                    # for interact
-                    x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
-                    x_axis_int[-1] = 0
-                    x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
-                    z_axis_int = np.array([0, 0, 1])
-                    y_axis_int = np.cross(z_axis_int, x_axis_int)
-                    y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
-                    transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
+                # if i == 0: 
+                #     # for interact
+                #     x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
+                #     x_axis_int[-1] = 0
+                #     x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
+                #     z_axis_int = np.array([0, 0, 1])
+                #     y_axis_int = np.cross(z_axis_int, x_axis_int)
+                #     y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
+                #     transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
 
-                    # for ego
-                    x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
-                    x_axis_ego[-1] = 0
-                    x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
-                    z_axis_ego = np.array([0, 0, 1])
-                    y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
-                    y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
-                    transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
+                #     # for ego
+                #     x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
+                #     x_axis_ego[-1] = 0
+                #     x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
+                #     z_axis_ego = np.array([0, 0, 1])
+                #     y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
+                #     y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
+                #     transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
 
-                joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
-                joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
-                # joints_3d = joints_3d - joints_3d[39]  # 4 is the root
-                # joints_3d_ego = joints_3d_ego - joints_3d_ego[39]  # 4 is the root
+                # joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
+                # joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+                joints_3d = joints_3d - joints_3d[39]  # 4 is the root
+                joints_3d_ego = joints_3d_ego - joints_3d_ego[39]  # 4 is the root
 
                 # j3ds[i] = joints_3d
                 # j2ds[i] = joints_2d
@@ -395,29 +395,29 @@ def read_val_data(dataset_path, device, data_type, debug=False):
                 #     bbox = get_bbox_from_kp2d(joints_2d[~np.all(joints_2d == 0, axis=1)]).reshape(4)
                 bbox = np.array([113, 113, w, h])  # shape = (4,N)
 
-                if i == 0: 
-                    # for interact
-                    x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
-                    x_axis_int[-1] = 0
-                    x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
-                    z_axis_int = np.array([0, 0, 1])
-                    y_axis_int = np.cross(z_axis_int, x_axis_int)
-                    y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
-                    transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
+                # if i == 0: 
+                #     # for interact
+                #     x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
+                #     x_axis_int[-1] = 0
+                #     x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
+                #     z_axis_int = np.array([0, 0, 1])
+                #     y_axis_int = np.cross(z_axis_int, x_axis_int)
+                #     y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
+                #     transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
 
-                    # for ego
-                    x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
-                    x_axis_ego[-1] = 0
-                    x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
-                    z_axis_ego = np.array([0, 0, 1])
-                    y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
-                    y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
-                    transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
+                #     # for ego
+                #     x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
+                #     x_axis_ego[-1] = 0
+                #     x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
+                #     z_axis_ego = np.array([0, 0, 1])
+                #     y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
+                #     y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
+                #     transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
 
-                joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
-                joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
-                # joints_3d = joints_3d - joints_3d[39]  # 4 is the root
-                # joints_3d_ego = joints_3d_ego - joints_3d_ego[39]  # 4 is the root
+                # joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
+                # joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+                joints_3d = joints_3d - joints_3d[39]  # 4 is the root
+                joints_3d_ego = joints_3d_ego - joints_3d_ego[39]  # 4 is the root
 
                 # j3ds[i] = joints_3d
                 # j2ds[i] = joints_2d
@@ -580,27 +580,29 @@ def read_train_data(dataset_path, device, data_type, debug=False):
                 joints_3d_ego = convert_kps(joints_3d_ego_raw, "you2me_cmu_3d", "spin").reshape((-1, 3))
                 # need to renormalize all sequence based on first frame rotation
                 # get x axis between two hip
-                if i == 0: 
-                    # for interact
-                    x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
-                    x_axis_int[-1] = 0
-                    x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
-                    z_axis_int = np.array([0, 0, 1])
-                    y_axis_int = np.cross(z_axis_int, x_axis_int)
-                    y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
-                    transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
+                # if i == 0: 
+                #     # for interact
+                #     x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
+                #     x_axis_int[-1] = 0
+                #     x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
+                #     z_axis_int = np.array([0, 0, 1])
+                #     y_axis_int = np.cross(z_axis_int, x_axis_int)
+                #     y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
+                #     transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
 
-                    # for ego
-                    x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
-                    x_axis_ego[-1] = 0
-                    x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
-                    z_axis_ego = np.array([0, 0, 1])
-                    y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
-                    y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
-                    transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
+                #     # for ego
+                #     x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
+                #     x_axis_ego[-1] = 0
+                #     x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
+                #     z_axis_ego = np.array([0, 0, 1])
+                #     y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
+                #     y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
+                #     transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
 
-                joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
-                joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+                # joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
+                # joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+                joints_3d = joints_3d - joints_3d[39]
+                joints_3d_ego = joints_3d_ego - joints_3d_ego[39]
 
                 #     bbox = get_bbox_from_kp2d(joints_2d[~np.all(joints_2d == 0, axis=1)]).reshape(4)
                 bbox = np.array([113, 113, w, h])  # shape = (4,N)
@@ -751,27 +753,30 @@ def read_train_data(dataset_path, device, data_type, debug=False):
                 # if joints_2d:
                 #     bbox = get_bbox_from_kp2d(joints_2d[~np.all(joints_2d == 0, axis=1)]).reshape(4)
 
-                if i == 0: 
-                    # for interact
-                    x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
-                    x_axis_int[-1] = 0
-                    x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
-                    z_axis_int = np.array([0, 0, 1])
-                    y_axis_int = np.cross(z_axis_int, x_axis_int)
-                    y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
-                    transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
+                # if i == 0: 
+                #     # for interact
+                #     x_axis_int = joints_3d[27, :] - joints_3d[28, :]  # [3] right hip - left hip
+                #     x_axis_int[-1] = 0
+                #     x_axis_int = x_axis_int / np.linalg.norm(x_axis_int)
+                #     z_axis_int = np.array([0, 0, 1])
+                #     y_axis_int = np.cross(z_axis_int, x_axis_int)
+                #     y_axis_int = y_axis_int / np.linalg.norm(y_axis_int)
+                #     transf_rotmat_int = np.stack([x_axis_int, y_axis_int, z_axis_int], axis=1)  # [3, 3]
 
-                    # for ego
-                    x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
-                    x_axis_ego[-1] = 0
-                    x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
-                    z_axis_ego = np.array([0, 0, 1])
-                    y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
-                    y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
-                    transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
+                #     # for ego
+                #     x_axis_ego = joints_3d_ego[27, :] - joints_3d_ego[28, :]  # [3] right hip - left hip
+                #     x_axis_ego[-1] = 0
+                #     x_axis_ego = x_axis_ego / np.linalg.norm(x_axis_ego)
+                #     z_axis_ego = np.array([0, 0, 1])
+                #     y_axis_ego = np.cross(z_axis_ego, x_axis_ego)
+                #     y_axis_ego = y_axis_ego / np.linalg.norm(y_axis_ego)
+                #     transf_rotmat_ego = np.stack([x_axis_ego, y_axis_ego, z_axis_ego], axis=1)  # [3, 3]
 
-                joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
-                joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+                # joints_3d = np.matmul(joints_3d - joints_3d[39], transf_rotmat_int)  # [T(/bs), 25, 3]
+                # joints_3d_ego = np.matmul(joints_3d_ego - joints_3d_ego[39], transf_rotmat_ego) 
+
+                joints_3d = joints_3d - joints_3d[39]
+                joints_3d_ego = joints_3d_ego - joints_3d_ego[39]
 
                 bbox = np.array([113, 113, w, h])  # shape = (4,N)
                 # joints_3d = joints_3d - joints_3d[39]  # 0 is hip
